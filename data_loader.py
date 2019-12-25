2fa99c04b637e98205bfc31c89e0c7c07c598653
h3li05369 <akashsm@ce.iitr.ac.in>
2019-12-25 15:10:24 +0530

    removed sentiment and stopword functions from data_loader

--- data_loader.py
+++ data_loader.py
@@ -15,37 +15,6 @@ class TextDataset(Dataset):
 
         self.bow_size = bow_size
 
-    def _get_sentiment_words(self):
-        """
-        Returns all the sentiment words (positive and negative) 
-        which are excluded from the main vocab to form the BoW vocab
-        """
-        with open(file='../data/lexicon/positive-words.txt',
-                  mode='r', encoding='ISO-8859-1') as pos_sentiment_words_file,\
-            open(file='../data/lexicon/negative-words.txt',
-                 mode='r', encoding='ISO-8859-1') as neg_sentiment_words_file:
-            pos_words = pos_sentiment_words_file.readlines()
-            neg_words = neg_sentiment_words_file.readlines()
-            words = pos_words + neg_words
-        words = set(word.strip() for word in words)
-
-        return words
-
-    def get_stopwords():
-        """
-        Returns all the stopwords which excluded from the 
-        main vocab to form the BoW vocab
-        """
-        nltk_stopwords = set(stopwords.words('english'))
-        sklearn_stopwords = stop_words.ENGLISH_STOP_WORDS
-
-        all_stopwords = set()
-        all_stopwords |= spacy_stopwords
-        all_stopwords |= nltk_stopwords
-        all_stopwords |= sklearn_stopwords
-
-        return all_stopwords
-
     def _get_bow_representations(self, text_sequences):
         """
         Returns BOW representation of every sequence of the batch
