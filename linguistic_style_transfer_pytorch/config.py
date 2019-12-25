819243648ca7ee8cea9ab6d0c0652475156f51cd
h3li05369 <akashsm@ce.iitr.ac.in>
2019-12-25 19:02:01 +0530

    updated

--- linguistic_style_transfer_pytorch/config.py
+++ linguistic_style_transfer_pytorch/config.py
@@ -8,6 +8,8 @@ class Config:
         self.train_neg_reviews_file_path = "../data/raw/yelp_train_neg.txt"
         self.train_text_file_path = "../data/clean/yelp_train_data.txt"
         self.train_labels_file_path = "../data/clean/yelp_train_labels.txt"
+        self.pos_sentiment_file_path = "../data/lexicon/positive-words.txt"
+        self.neg_sentiment_file_path = "../data/lexicon/negative-words.txt"
         self.unk_token = "<unk>"
         self.sos_token = "<sos>"
         self.eos_token = "<eos>"
