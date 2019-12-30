ac4d11eee07151c3fc15f9cd3694f69683f4c75c
h3li05369 <akashsm@ce.iitr.ac.in>
2019-12-30 17:23:34 +0530

    implemented discriminator losses

--- linguistic_style_transfer_pytorch/model.py
+++ linguistic_style_transfer_pytorch/model.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
-from linguistic_style_transfer_pytorch.config import ModelConfig
+from linguistic_style_transfer_pytorch.config import ModelConfig, Config
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 
-config = ModelConfig()
+mconfig = ModelConfig()
+gconfig = Config()
 
 
 class AutoEncoder(nn.Module):
@@ -17,31 +18,118 @@ class AutoEncoder(nn.Module):
         """
         # Inference mode or training mode
         self.inference = inference
+        # word embeddings
+        self.embedding = nn.Embedding(
+            gconfig.vocab_size, mconfig.embedding_size)
         # Encoder model
         self.encoder = nn.GRU(
-            config.embedding_size, config.hidden_dim, batch_first=True, bidirectional=True)
+            mconfig.embedding_size, mconfig.hidden_dim, batch_first=True, bidirectional=True)
         # content latent embedding
-        self.content_emb = nn.Linear(
-            config.hidden_dim, config.content_hidden_dim)
+        self.get_content_emb = nn.Linear(
+            mconfig.hidden_dim, mconfig.content_hidden_dim)
         # style latent embedding
-        self.style_emb = nn.Linear(config.hidden_dim, config.style_hidden_dim)
+        self.get_style_emb = nn.Linear(
+            mconfig.hidden_dim, mconfig.style_hidden_dim)
         # Discriminator/adversary
-        self.content_disc = nn.Linear(
-            config.content_hidden_dim, config.num_style)
         self.style_disc = nn.Linear(
-            config.style_hidden_dim, config.content_bow_dim)
+            mconfig.content_hidden_dim, mconfig.num_style)
+        self.content_disc = nn.Linear(
+            mconfig.style_hidden_dim, mconfig.content_bow_dim)
         # classifier
         self.content_classifier = nn.Linear(
-            config.content_hidden_dim, config.content_bow_dim)
+            mconfig.content_hidden_dim, mconfig.content_bow_dim)
         self.style_classifier = nn.Linear(
-            config.style_hidden_dim, config.num_style)
+            mconfig.style_hidden_dim, mconfig.num_style)
         # Decoder
+        # Note: input embeddings are concatenated with the sampled latent vector at every step
         self.decoder = nn.GRUCell(
-            config.embedding_size, config.hidden_dim, batch_first=True, bidirectional=True)
+            mconfig.embedding_size + mconfig.content_hidden_dim + mconfig.style_hidden_dim, mconfig.hidden_dim)
         # dropout
-        self.dropout = config.emb_dropout
-        self.init()
+        self.dropout = mconfig.emb_dropout
 
-    def forward(self, sequences, seq_lengths, labels, content_bow):
+    def sample_prior(self, mu, log_sigma):
         """
+        Returns samples drawn from the latent space constrained to 
+        follow diagonal Gaussian
         """
+        epsilon = torch.randn(mu.size(1))
+        return mu + epsilon*torch.exp(log_sigma)
+
+    def get_content_disc_loss(self, style_emb, content_bow):
+        """
+        It essentially quantifies the amount of information about content  
+        contained in the style space
+        Returns:
+        cross entropy loss of content discriminator
+        """
+        # predictions
+        # Note: detach the style embedding since when don't want the gradient to flow
+        #       all the way to the encoder. content_disc_loss is used only to change the
+        #       parameters of the discriminator network
+        preds = nn.Softmax(self.content_disc(self.dropout(style_emb.detach())))
+        # label smoothing
+        smoothed_content_bow = content_bow * \
+            (1-mconfig.label_smoothing) + \
+            mconfig.label_smoothing/mconfig.content_bow_dim
+        # calculate cross entropy loss
+        content_disc_loss = nn.BCELoss(preds, smoothed_content_bow.detach())
+
+        return content_disc_loss
+
+    def get_style_disc_loss(self, content_emb, style_labels):
+        """
+        It essentially quantifies the amount of information about style  
+        contained in the content space
+        Returns:
+        cross entropy loss of style discriminator
+        """
+        # predictions
+        # Note: detach the content embedding since when don't want the gradient to flow
+        #       all the way to the encoder. style_disc_loss is used only to change the
+        #       parameters of the discriminator network
+        preds = nn.Softmax(self.style_disc(self.dropout(content_emb.detach())))
+        # label smoothing
+        smoothed_style_labels = style_labels * \
+            (1-mconfig.label_smoothing) + \
+            mconfig.label_smoothing/mconfig.num_style
+        # calculate cross entropy loss
+        style_disc_loss = nn.BCELoss(preds, smoothed_style_labels.detach())
+
+        return style_disc_loss
+
+    def get_content_mul_loss(self, content_emb, content_bow):
+        """
+        This loss quantifies the amount of content information preserved 
+        in the content space
+        Returns:
+        cross entropy loss of the content classifier
+        """
+        # predictions
+        preds = nn.Softmax(self.content_classifier(self.dropout(content_emb)))
+
+    def forward(self, sequences, seq_lengths, style_labels, content_bow):
+        """
+        Returns the logits
+        """
+        embedded_seqs = self.dropout(self.embedding(sequences))
+        # pack the sequences to reduce unnecessary computations
+        packed_seqs = pack_padded_sequence(
+            embedded_seqs, lengths=seq_lengths, batch_first=True)
+        packed_output, (final_hidden_state,
+                        final_cell_state) = self.encoder(packed_seqs)
+        # get content and style embeddings from the sentence embeddings,i.e. final_hidden_state
+        content_emb_mu, content_emb_sigma = self.get_content_emb(
+            final_hidden_state)
+        style_emb_mu, style_emb_sigma = self.get_style_emb(final_hidden_state)
+        # sample content and style embeddings from their respective latent spaces
+        sampled_content_emb = self.sample_prior(
+            content_emb_mu, content_emb_sigma)
+        sampled_style_emb = self.sample_prior(style_emb_mu, style_emb_sigma)
+
+        #### Losses on content space ###
+        # Discriminator Loss
+        content_disc_loss = self.get_content_disc_loss(
+            sampled_style_emb, content_bow)
+        # Multitask loss
+        content_mul_loss = self.get_content_mul_loss(
+            sampled_content_emb, content_bow)
