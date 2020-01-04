3a2db24370f7faef0cc7b61db4d822c68ee7f5ea
h3li05369 <akashsm@ce.iitr.ac.in>
2020-01-04 18:34:48 +0530

    major bug fixes

--- linguistic_style_transfer_pytorch/model.py
+++ linguistic_style_transfer_pytorch/model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from linguistic_style_transfer_pytorch.config import ModelConfig, GeneralConfig
-from torch.nn.utils.rnn import pack_padded_sequence
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 import math
 
 mconfig = ModelConfig()
@@ -31,11 +31,15 @@ class AdversarialVAE(nn.Module):
         self.encoder = nn.GRU(
             mconfig.embedding_size, mconfig.hidden_dim, batch_first=True, bidirectional=True)
         # content latent embedding
-        self.get_content_emb = nn.Linear(
-            mconfig.hidden_dim, mconfig.content_hidden_dim)
+        self.content_mu = nn.Linear(
+            2*mconfig.hidden_dim, mconfig.content_hidden_dim)
+        self.content_log_var = nn.Linear(
+            2*mconfig.hidden_dim, mconfig.content_hidden_dim)
         # style latent embedding
-        self.get_style_emb = nn.Linear(
-            mconfig.hidden_dim, mconfig.style_hidden_dim)
+        self.style_mu = nn.Linear(
+            2*mconfig.hidden_dim, mconfig.style_hidden_dim)
+        self.style_log_var = nn.Linear(
+            2*mconfig.hidden_dim, mconfig.style_hidden_dim)
         #=============== Discriminator/adversary============#
         self.style_disc = nn.Linear(
             mconfig.content_hidden_dim, mconfig.num_style)
@@ -77,22 +81,22 @@ class AdversarialVAE(nn.Module):
         embedded_seqs = self.dropout(self.embedding(sequences))
         packed_seqs = pack_padded_sequence(
             embedded_seqs, lengths=seq_lengths, batch_first=True)
-        packed_output, (final_hidden_state,
-                        final_cell_state) = self.encoder(packed_seqs)
+        packed_output, (_) = self.encoder(packed_seqs)
+        output, _ = pad_packed_sequence(packed_output, batch_first=True)
+        sentence_emb = output[torch.arange(output.size(0)), seq_lengths-1]
         # get content and style embeddings from the sentence embeddings,i.e. final_hidden_state
-        print("final_h_shape ", final_hidden_state.shape)
-        content_emb_mu, content_emb_log_sigma = self.get_content_emb(
-            final_hidden_state)
-        style_emb_mu, style_emb_log_sigma = self.get_style_emb(
-            final_hidden_state)
+        content_emb_mu, content_emb_log_var = self.get_content_emb(
+            sentence_emb)
+        style_emb_mu, style_emb_log_var = self.get_style_emb(
+            sentence_emb)
         # sample content and style embeddings from their respective latent spaces
         sampled_content_emb = self.sample_prior(
-            content_emb_mu, content_emb_log_sigma)
+            content_emb_mu, content_emb_log_var)
         sampled_style_emb = self.sample_prior(
-            style_emb_mu, style_emb_log_sigma)
+            style_emb_mu, style_emb_log_var)
         # Generative embedding
         generative_emb = torch.cat(
-            sampled_style_emb, sampled_content_emb, axis=1)
+            (sampled_style_emb, sampled_content_emb), axis=1)
 
         #=========== Losses on content space =============#
         # Discriminator Loss
@@ -119,13 +123,13 @@ class AdversarialVAE(nn.Module):
         #============== KL losses ===========#
         # Style space
         style_kl_loss = self.get_kl_loss(
-            style_emb_mu, style_emb_log_sigma)
+            style_emb_mu, style_emb_log_var)
         if iteration < mconfig.kl_anneal_iterations:
             style_kl_loss = self.get_annealed_weight(
                 iteration, mconfig.style_kl_lambda) * style_kl_loss
         # Content space
         content_kl_loss = self.get_kl_loss(
-            content_emb_mu, content_emb_log_sigma)
+            content_emb_mu, content_emb_log_var)
         if iteration < mconfig.kl_anneal_iterations:
             content_kl_loss = self.get_annealed_weight(
                 iteration, mconfig.content_kl_lambda) * content_kl_loss
@@ -160,13 +164,39 @@ class AdversarialVAE(nn.Module):
 
         return content_disc_params, style_disc_params, other_params
 
-    def sample_prior(self, mu, log_sigma):
+    def get_content_emb(self, sentence_emb):
+        """
+        Args:
+            sentence_emb: sentence embeddings of all the sentences in the batch, shape=(batch_size,2*gru_hidden_dim)
+        Returns:
+            mu: embedding of the mean of the Gaussian distribution of the content's latent space
+            log_var: embedding of the log of variance of the Gaussian distribution of the content's latent space
+        """
+        mu = self.content_mu(sentence_emb)
+        log_var = self.content_log_var(sentence_emb)
+
+        return mu, log_var
+
+    def get_style_emb(self, sentence_emb):
+        """
+        Args:
+            sentence_emb: sentence embeddings of all the sentences in the batch, shape=(batch_size,2*gru_hidden_dim)
+        Returns:
+            mu: embedding of the mean of the Gaussian distribution of the style's latent space
+            log_var: embedding of the log of variance of the Gaussian distribution of the style's latent space
+        """
+        mu = self.style_mu(sentence_emb)
+        log_var = self.style_log_var(sentence_emb)
+
+        return mu, log_var
+
+    def sample_prior(self, mu, log_var):
         """
         Returns samples drawn from the latent space constrained to
         follow diagonal Gaussian
         """
         epsilon = torch.randn(mu.size(1))
-        return mu + epsilon*torch.exp(log_sigma)
+        return mu + epsilon*torch.exp(log_var)
 
     def get_content_disc_preds(self, style_emb):
         """
@@ -178,7 +208,7 @@ class AdversarialVAE(nn.Module):
         # Note: detach the style embedding since when don't want the gradient to flow
         #       all the way to the encoder. content_disc_loss is used only to change the
         #       parameters of the discriminator network
-        preds = nn.Softmax(self.content_disc(self.dropout(style_emb.detach())))
+        preds = nn.Softmax()(self.content_disc(self.dropout(style_emb.detach())))
 
         return preds
 
@@ -194,8 +224,8 @@ class AdversarialVAE(nn.Module):
             (1-mconfig.label_smoothing) + \
             mconfig.label_smoothing/mconfig.content_bow_dim
         # calculate cross entropy loss
-        content_disc_loss = nn.BCELoss(
-            content_disc_preds, smoothed_content_bow)
+        print(smoothed_content_bow.shape, content_disc_preds.shape)
+        content_disc_loss = nn.BCELoss()(content_disc_preds, smoothed_content_bow)
 
         return content_disc_loss
 
@@ -209,7 +239,7 @@ class AdversarialVAE(nn.Module):
         # Note: detach the content embedding since when don't want the gradient to flow
         #       all the way to the encoder. style_disc_loss is used only to change the
         #       parameters of the discriminator network
-        preds = nn.Softmax(self.style_disc(self.dropout(content_emb.detach())))
+        preds = nn.Softmax()(self.style_disc(self.dropout(content_emb.detach())))
 
         return preds
 
@@ -225,7 +255,8 @@ class AdversarialVAE(nn.Module):
             (1-mconfig.label_smoothing) + \
             mconfig.label_smoothing/mconfig.num_style
         # calculate cross entropy loss
-        style_disc_loss = nn.BCELoss(style_disc_preds, smoothed_style_labels)
+        print("shapes ", smoothed_style_labels.shape, style_disc_preds.shape)
+        style_disc_loss = nn.BCELoss()(style_disc_preds, smoothed_style_labels)
 
         return style_disc_loss
 
@@ -244,13 +275,13 @@ class AdversarialVAE(nn.Module):
         cross entropy loss of the content classifier
         """
         # predictions
-        preds = nn.Softmax(self.content_classifier(self.dropout(content_emb)))
+        preds = nn.Softmax()(self.content_classifier(self.dropout(content_emb)))
         # label smoothing
         smoothed_content_bow = content_bow * \
             (1-mconfig.label_smoothing) + \
             mconfig.label_smoothing/mconfig.content_bow_dim
         # calculate cross entropy loss
-        content_mul_loss = nn.BCELoss(preds, smoothed_content_bow)
+        content_mul_loss = nn.BCELoss()(preds, smoothed_content_bow)
 
         return content_mul_loss
 
@@ -262,7 +293,7 @@ class AdversarialVAE(nn.Module):
         cross entropy loss of the style classifier
         """
         # predictions
-        preds = nn.Softmax(self.content_classifier(self.dropout(style_emb)))
+        preds = nn.Softmax()(self.style_classifier(self.dropout(style_emb)))
         # label smoothing
         smoothed_style_labels = style_labels * \
             (1-mconfig.label_smoothing) + \
@@ -285,16 +316,16 @@ class AdversarialVAE(nn.Module):
             (mconfig.kl_anneal_iterations / 3))
             + 1) * lambda_weight
 
-    def get_kl_loss(self, mu, log_sigma):
+    def get_kl_loss(self, mu, log_var):
         """
         Args:
             mu: batch of means of the gaussian distribution followed by the latent variables
-            log_sigma: batch of log variances(log_sigma) of the gaussian distribution followed by the latent variables
+            log_var: batch of log variances(log_var) of the gaussian distribution followed by the latent variables
         Returns:
             total loss(float)
         """
-        kl_loss = torch.mean((-0.5*torch.sum(1+log_sigma -
-                                             log_sigma.exp()-mu.pow(2), dim=1)))
+        kl_loss = torch.mean((-0.5*torch.sum(1+log_var -
+                                             log_var.exp()-mu.pow(2), dim=1)))
         return kl_loss
 
     def generate_sentences(self, input_sentences, latent_emb):
@@ -322,7 +353,7 @@ class AdversarialVAE(nn.Module):
             latent_emb = latent_emb.unsqueeze(1).repeat(
                 1, mconfig.max_seq_len, 1)
             gen_sent_embs = torch.cat(
-                sentence_embs, latent_emb, dim=2)
+                (sentence_embs, latent_emb), dim=2)
             # Delete latent embedding and sos token tensor to reduce memory usage
             del latent_emb, sos_token_tensor
             output_sentences = torch.zeros(
