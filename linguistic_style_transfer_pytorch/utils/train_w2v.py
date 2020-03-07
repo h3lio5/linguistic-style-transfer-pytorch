00616b73f2e76eabaa73d2d3cb661a4a546a4958
Akash S M <47632450+h3lio5@users.noreply.github.com>
2020-03-08 02:38:12 +0530

    Update train_w2v.py

--- linguistic_style_transfer_pytorch/utils/train_w2v.py
+++ linguistic_style_transfer_pytorch/utils/train_w2v.py
@@ -3,12 +3,12 @@ from gensim.models.word2vec import LineSentence
 from linguistic_style_transfer_pytorch.config import GeneralConfig
 
 
-def train_word2vec_model(text_file_path, model_file_path):
+def train_word2vec_model(text_file_path, model_file_path,embedding_size):
     # define training data
     # train model
     print("Loading input file and training mode ...")
     model = Word2Vec(sentences=LineSentence(text_file_path),
-                     min_count=1, size=config.embedding_size)
+                     min_count=1, size=embedding_size)
     # summarize the loaded model
     print("Model Details: {}".format(model))
     # save model
@@ -19,4 +19,4 @@ def train_word2vec_model(text_file_path, model_file_path):
 if __name__ == "__main__":
     gconfig = GeneralConfig()
     train_word2vec_model(gconfig.train_text_file_path,
-                         gconfig.word_embedding_text_file_path)
+                         gconfig.word_embedding_text_file_path,gconfig.embedding_size)
